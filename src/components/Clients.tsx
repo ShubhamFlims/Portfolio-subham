a5469b7c73c757861a247dadeec2ea8c7ab547a9
ShubhamFlims <shubhammaner1235@gmail.com>
2026-08-18 15:37:13 +0530

    Update client logos and names in Clients.tsx

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,24 +1,23 @@
 import { motion } from "motion/react";
 
 const clients = [
-  { name: "Client 1", logo: "/clients/client-1.png" },
-  { name: "Client 2", logo: "/clients/client-2.png" },
-  { name: "Client 3", logo: "/clients/client-3.png" },
-  { name: "Client 4", logo: "/clients/client-4.png" },
-  { name: "Client 5", logo: "/clients/client-5.png" },
-  { name: "Client 6", logo: "/clients/client-6.png" },
-  { name: "Client 7", logo: "/clients/client-7.png" },
-  { name: "Client 8", logo: "/clients/client-8.png" },
-  { name: "Client 9", logo: "/clients/client-9.png" },
-  { name: "Client 10", logo: "/clients/client-10.png" },
-  { name: "Client 11", logo: "/clients/client-11.png" },
-  { name: "Client 12", logo: "/clients/client-12.png" },
-  { name: "Client 13", logo: "/clients/client-13.png" },
-  { name: "Client 14", logo: "/clients/client-14.png" },
-  { name: "Client 15", logo: "/clients/client-15.png" },
-  { name: "Client 16", logo: "/clients/client-16.png" },
+  { name: "Client 1", logo: "/2.png" },
+  { name: "Client 2", logo: "/356782075_3228027087490457_7985763114042100718_n.jpg" },
+  { name: "Client 3", logo: "/412689038_672229138131377_5864501099487066219_n.jpg" },
+  { name: "Client 4", logo: "/441819772_439700032082052_656436152564400092_n.jpg" },
+  { name: "Client 5", logo: "/534777593_18131528935450444_6802202317043440863_n.jpg" },
+  { name: "Client 6", logo: "/536533614_17863756659448696_596343483044701658_n.jpg" },
+  { name: "Client 7", logo: "/538707630_18107964259544542_5729342916690522184_n.jpg" },
+  { name: "Client 8", logo: "/652799791_18060205394402004_5523147764040525668_n.jpg" },
+  { name: "Client 9", logo: "/Artboard 1 copy.png" },
+  { name: "Client 10", logo: "/ChatGPT Image Jul 21, 2026, 03_05_19 PM.png" },
+  { name: "Client 11", logo: "/ChatGPT Image Jun 10, 2026, 12_06_09 AM (1).png" },
+  { name: "Client 12", logo: "/ChatGPT Image May 31, 2026, 11_02_54 PM.png" },
+  { name: "Client 13", logo: "/Naturals.png" },
+  { name: "Client 14", logo: "/White_Logo_no_BG.png" },
+  { name: "Client 15", logo: "/White_Logo_no_bg (2).png" },
+  { name: "Client 16", logo: "/png sweatbox logo.png" },
 ];
-
 export function Clients() {
   const logos = [...clients, ...clients];
 
